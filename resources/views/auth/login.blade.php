70a3c36355d3cf846605835fed9f055a9feb5347
DimasPradana <deemaazbaek@gmail.com>
2021-11-10 08:16:27 +0700

    change grey to green on login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,12 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
+{{--                <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>--}}
+                <x-application-logo class="w-20 h-20 fill-current text-green-500"/>
             </a>
         </x-slot>
+
+
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')"/>
