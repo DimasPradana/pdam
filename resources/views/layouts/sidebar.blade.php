ad0e7939a7a66361bdfca4cc4c159ca7af2f1d9b
DimasPradana <deemaazbaek@gmail.com>
2021-11-30 07:43:09 +0700

    change title

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,8 +6,7 @@
         {{-- header kiri--}}
         <div class="flex flex-row items-center justify-between flex-shrink-0 px-8 py-4">
             <a href="#"
-               class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark:text-white focus:outline-none focus:shadow-outline">PDAM
-                GIS</a>
+               class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark:text-white focus:outline-none focus:shadow-outline">Geographics Information System</a>
             <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
                 <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
                     <path x-show="!open" fill-rule="evenodd"
